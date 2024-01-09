8af8657ac6c16420a66cb4c050634cfdca6ccff5
Tymur1990 <89634574+Tymur1990@users.noreply.github.com>
2024-01-09 15:25:43 +0200

    HW_2

--- main.js
+++ main.js
@@ -1,58 +1,110 @@
+const express = require('express');
 const path = require('path')
 const fs = require('fs')
-const mainPath = path.join(__dirname, 'files')
 
-fs.mkdir(mainPath, (err) => {
-    if (err) throw err;
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+const mainPath = path.join(__dirname, 'users.json')
+
+let users = []
+
+fs.readFile(mainPath, (err, data) => {
+    if (err) {
+        console.log(err);
+    }
+    users = JSON.parse(data)
 })
 
-for (let i = 1; i <= 5; i++) {
-    fs.mkdir(path.join(mainPath, `data_${i}`), () => {})
-    fs.writeFile(path.join(mainPath, `file_${i}`), `this is text for file${i}`, () => {})
-}
-
-fs.readdirSync(mainPath).forEach(item => {
-    const itemPath = path.join(mainPath, item)
-    fs.stat(itemPath, (err, stats) => {
-        if (stats.isDirectory()) {
-            console.log(`Directory: ${path.basename(itemPath)}`);
-        } else if (stats.isFile()) {
-            console.log(`FILE: ${path.basename(itemPath)}`);
+app.get('/users', (req, res) => {
+    fs.readFile(mainPath, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Problem with server file');
         }
+        res.json(JSON.parse(data))
+        users = JSON.parse(data)
     })
 })
 
+app.get('/users/:userId', (req, res) => {
+    const {userId} = req.params;
 
-// fs.mkdir(mainPath, (err) => {
-//     if (err) throw err;
-//     for (let i = 1; i <= 5; i++) {
-//         fs.mkdir(path.join(mainPath, `data_${i}`), () => {})
-//         fs.writeFile(path.join(mainPath, `file_${i}`), `this is text for file${i}`, () => {})
-//     }
-//
-//     let pathsToCheck = []
-//     for (let i = 1; i <= 5; i++) {
-//         pathsToCheck.push(path.join(mainPath, `data_${i}`))
-//         pathsToCheck.push(path.join(mainPath, `file_${i}`))
-//     }
-//
-//     for (let i = 0; i < pathsToCheck.length; i++) {
-//         const normPath = path.normalize(pathsToCheck[i])
-//         fs.stat(normPath, (err, stats) => {
-//             if (err) throw err;
-//             if (stats.isDirectory()) {
-//                 console.log(`Directory: ${path.basename(normPath)}`);
-//             } else if (stats.isFile()) {
-//                 console.log(`FILE: ${path.basename(normPath)}`);
-//             }
-//         });
-//     }
-// })
+    const userIndex = users.findIndex((user) => user.id === +userId);
+    console.log(userIndex);
+    if (userIndex > 0) {
+        res.json(users[userIndex])
+    } else {
+        res.send('no User with this id')
+    }
+})
 
 
+app.post('/users', (req, res) => {
+    const newData = req.body;
 
+    if (newData.name.length > 3 && newData.age > 0) {
+        users.push(newData)
+        fs.writeFile(mainPath, JSON.stringify(users), (err) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send('Problem with server file')
+            }
+            res.send('User Add to file')
+        })
+    } else {
+        res.status(400).send('User name must be > 3 letters and age must be > 0')
+    }
+})
 
+app.delete('/users/:userId', (req, res) => {
+    const {userId} = req.params;
 
+    const userIndex = users.findIndex((user) => user.id === +userId);
 
+    if (userIndex > 0) {
+        users.splice(userIndex, 1)
 
+        fs.writeFile(mainPath, JSON.stringify(users), (err) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send('Problem with server file')
+            } else {
+                res.status(200).send('User deleted')
+            }
+        })
+    } else {
+        res.status(400).send('User is not found')
+    }
+})
+
+app.put('/users/:userId', (req, res) => {
+    const {userId} = req.params;
+    const newData = req.body;
+
+    const userIndex = users.findIndex((user) => user.id === +userId);
+
+    if (userIndex > 0) {
+        users[userIndex] = newData
+
+        fs.writeFile(mainPath, JSON.stringify(users), (err) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send('Problem with server file')
+            } else {
+                res.status(200).send('User replace')
+            }
+        })
+    } else {
+        res.status(400).send('User is not found')
+    }
+
+})
+
+app.listen(port, () => {
+    console.log(`Сервер слухає на порту ${port}`);
+});
 
