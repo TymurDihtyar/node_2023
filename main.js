772f1e2b34198bad246c446517de962198216f69
Tymur1990 <89634574+Tymur1990@users.noreply.github.com>
2024-01-11 15:25:13 +0200

    HW_3

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const path = require('path')
-const fs = require('fs')
+const {read, write} = require('./fs.service')
 
 const app = express();
 const port = 3000;
@@ -8,103 +7,110 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
-const mainPath = path.join(__dirname, 'users.json')
-
-let users = []
-
-fs.readFile(mainPath, (err, data) => {
-    if (err) {
-        console.log(err);
+app.get('/users', async (req, res) => {
+    try {
+        const users = await read();
+        res.status(200).json(users)
+    } catch (e) {
+        res.status(400).json(e.message)
     }
-    users = JSON.parse(data)
 })
 
-app.get('/users', (req, res) => {
-    fs.readFile(mainPath, (err, data) => {
-        if (err) {
-            console.log(err);
-            res.status(500).send('Problem with server file');
+app.get('/users/:id', async (req, res) => {
+    try {
+        const id = Number(req.params.id);
+        if (!Number.isInteger(id)) {
+            throw new Error('wrong ID param')
         }
-        res.json(JSON.parse(data))
-        users = JSON.parse(data)
-    })
-})
 
-app.get('/users/:userId', (req, res) => {
-    const {userId} = req.params;
-
-    const userIndex = users.findIndex((user) => user.id === +userId);
-
-    if (userIndex >= 0) {
-        res.json(users[userIndex])
-    } else {
-        res.send('no User with this id')
-    }
-})
-
-
-app.post('/users', (req, res) => {
-    const newData = req.body;
-
-    if (newData.name.length >= 3 && newData.age > 0) {
-        users.push(newData)
-        fs.writeFile(mainPath, JSON.stringify(users), (err) => {
-            if (err) {
-                console.log(err);
-                res.status(500).send('Problem with server file')
-            }
-            res.send('User Add to file')
-        })
-    } else {
-        res.status(400).send('User name must have > 3 letters and age must be > 0')
-    }
-})
-
-app.delete('/users/:userId', (req, res) => {
-    const {userId} = req.params;
-
-    const userIndex = users.findIndex((user) => user.id === +userId);
-
-    if (userIndex > 0) {
-        users.splice(userIndex, 1)
-
-        fs.writeFile(mainPath, JSON.stringify(users), (err) => {
-            if (err) {
-                console.log(err);
-                res.status(500).send('Problem with server file')
-            } else {
-                res.status(200).send('User deleted')
-            }
-        })
-    } else {
-        res.status(400).send('User is not found')
-    }
-})
-
-app.put('/users/:userId', (req, res) => {
-    const {userId} = req.params;
-    const newData = req.body;
-
-    if (newData.name.length >= 3 && newData.age > 0) {
-
-        const userIndex = users.findIndex((user) => user.id === +userId);
-
-        if (userIndex > 0) {
-            users[userIndex] = newData
-
-            fs.writeFile(mainPath, JSON.stringify(users), (err) => {
-                if (err) {
-                    console.log(err);
-                    res.status(500).send('Problem with server file')
-                } else {
-                    res.status(200).send('User replace')
-                }
-            })
-        } else {
-            res.status(400).send('User is not found')
+        const users = await read();
+        const index = users.findIndex((user) => user.id === id)
+        if (index === -1) {
+            throw new Error('user not found')
         }
-    } else {
-        res.status(400).send('User name must have > 3 letters and age must be > 0')
+        res.status(200).json(users[index])
+    } catch (e) {
+        res.status(400).json(e.message)
+    }
+})
+
+app.post('/users', async (req, res) => {
+    try {
+        const {name, age, email} = req.body;
+        if (!age || !Number.isInteger(age) || age < 0 || age > 100) {
+            throw new Error('wrong age')
+        }
+        if (!email || !email.includes('@')) {
+            throw new Error('wrong email')
+        }
+        if (!name || name.length <= 3) {
+            throw new Error('wrong name')
+        }
+
+        const users = await read();
+        const newUser = {id: users[users.length - 1].id + 1, name, age, email}
+        users.push(newUser);
+
+        await write(users);
+        res.status(201).json(newUser)
+    } catch (e) {
+        res.status(400).json(e.message)
+    }
+})
+
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const id = Number(req.params.id);
+
+        if (!Number.isInteger(id)) {
+            throw new Error('wrong ID param')
+        }
+
+        const users = await read();
+        const index = users.findIndex((user) => user.id === id)
+        if (index === -1) {
+            throw new Error('user not found')
+        }
+        users.splice(index, 1)
+        await write(users)
+        res.status(204).json("User deleted")
+    } catch (e) {
+        res.status(400).json(e.message)
+    }
+})
+
+app.put('/users/:id', async (req, res) => {
+    try {
+        const id = Number(req.params.id);
+        const {name, age, email} = req.body;
+
+        if (!Number.isInteger(id)) {
+            throw new Error('wrong ID param')
+        }
+        if (!age || !Number.isInteger(age) || age < 0 || age > 100) {
+            throw new Error('wrong age')
+        }
+        if (!email || !email.includes('@')) {
+            throw new Error('wrong email')
+        }
+        if (!name || name.length <= 3) {
+            throw new Error('wrong name')
+        }
+
+        const users = await read();
+        const user = users.find((user) => user.id === id)
+        if (!user) {
+            throw new Error('user not found')
+        }
+
+        user.name = name
+        user.age = age
+        user.email = email
+
+        await write(users)
+        res.status(201).json(user)
+    } catch (e) {
+        res.status(400).json(e.message)
     }
 })
 
