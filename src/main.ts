aca37366122a2b181998a51e22c94559d5504ace
Tymur1990 <89634574+Tymur1990@users.noreply.github.com>
2024-01-28 13:12:21 +0200

    HW_8.1

--- src/main.ts
+++ src/main.ts
@@ -14,15 +14,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/auth", authRouter);
 app.use("/users", userRouter);
 
-app.use(
-  "*",
-  (err: ApiError, req: Request, res: Response, next: NextFunction) => {
-    return res.status(err?.status || 500).json({
-      message: err?.message,
-      status: err?.status,
-    });
-  },
-);
+app.use("*", (err: ApiError, req: Request, res: Response, next: NextFunction) => {
+  return res.status(err?.status || 500).json({
+    message: err?.message,
+    status: err?.status,
+  });
+});
 
 const PORT = configs.PORT;
 app.listen(PORT, async () => {
